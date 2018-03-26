ab00b5397775e642e854dc7c2eb4d3c7a23f9f8b
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2018-03-26 18:19:48 +0300

    Fixing an issue which allowed index name collisions to happen in postgre

--- src/Migrations/StreamlinedInstallMigration.php
+++ src/Migrations/StreamlinedInstallMigration.php
@@ -24,7 +24,7 @@ abstract class StreamlinedInstallMigration extends Migration
 
             foreach ($table->getIndexes() as $index) {
                 $this->createIndex(
-                    $index->getName(),
+                    $table->getName() . '_' . $index->getName(),
                     $table->getDatabaseName(),
                     $index->getColumns(),
                     $index->isUnique()
