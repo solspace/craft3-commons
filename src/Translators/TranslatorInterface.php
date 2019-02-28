15573dbe54b63a274ed6fdec2bd810b9f0e4866d
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2019-02-28 11:22:54 +0200

    Adding translator interface and implementation

---
+++ src/Translators/TranslatorInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Solspace\Commons\Translators;
+
+interface TranslatorInterface
+{
+    /**
+     * Translates a string
+     * Replaces any variables in the $string with variables from $variables
+     * User brackets to specify variables in string
+     *
+     * Example:
+     * Translation string: "Hello, {firstName}!"
+     * Variables: ["firstName": "Icarus"]
+     * End result: "Hello, Icarus!"
+     *
+     * @param string $category
+     * @param string $string
+     * @param array  $variables
+     *
+     * @return string
+     */
+    public function translate(string $category, string $string, array $variables = []): string;
+}
